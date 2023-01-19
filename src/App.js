af38fa70fbbbfec69d67d7fbea4a114c09725d6b
Janier Hernandez <janier007@gmail.com>
2023-01-19 14:39:27 -0500

    curso-platzi-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+import { TodoList } from './TodoList';
+
+const todos = [
+	{text: 'cortar cebolla', completed: false},
+	{text: 'tomar el curso de react', completed: false},
+	{text: 'llorar con la llorona', completed: false}
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<TodoCounter/>
+
+			<TodoSearch/>
+			<TodoList>
+				{todos.map( todo => (
+					<TodoItem key={todo.text} text={todo.text}/>
+				))}
+			</TodoList>
+			<CreateTodoButton/>
+		</>
+	);
 }
 
 export default App;
---
+++ src/TodoItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TodoItem (props) {
+    return(
+        <li>
+            <span>C</span>
+            <p>{props.text}</p>
+            <span>x</span>
+        </li>
+    )
+}
+export { TodoItem };
